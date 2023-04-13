8b66f556da14fa5df595cfc7906b84242bbfcd0d
ferbogorni <ferbogorni@gmail.com>
2023-04-13 20:41:09 -0300

    cambios 13 de abril

--- src/rpart/z231_mejormodelo.r
+++ src/rpart/z231_mejormodelo.r
@@ -9,7 +9,7 @@ require("data.table")
 require("rpart")
 require("parallel")
 
-ksemillas  <- c( 102191, 200177, 410551, 552581, 892237 ) #reemplazar por las propias semillas
+ksemillas  <- c( 100103, 100109, 100129, 100151, 100153 ) #reemplazar por las propias semillas
 
 #------------------------------------------------------------------------------
 #particionar agrega una columna llamada fold a un dataset que consiste en una particion estratificada segun agrupa
@@ -74,7 +74,7 @@ ArbolesMontecarlo  <- function( semillas,  param_basicos )
 #------------------------------------------------------------------------------
 
 #Aqui se debe poner la carpeta de la computadora local
-setwd("X:\\gdrive\\austral2023v\\")   #Establezco el Working Directory
+setwd("C:/Users/ferbo/Documents/Maestria/7_LI1")   #Establezco el Working Directory
 #cargo los datos
 
 #cargo los datos
